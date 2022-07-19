dcbdb3f94e0cb1a82233ea2a22e61a6246d2dd8a
Trung Nguyen <trungnguyen2809@gmail.com>
2022-07-19 11:09:33 -0500

    setup mongodb apolloClient apolloProvider

--- client/index.js
+++ client/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+// empty config object is passed -> use default configuration (by default graphQL server is on the /graphql)
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
